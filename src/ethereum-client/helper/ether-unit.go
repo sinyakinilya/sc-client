0dcc820271eb0f1ee578a567bdd8807c90da46cd
Ilya Sinyakin <sinyakin.ilya@gmail.com>
2018-02-15 20:12:48 +0200

    helper.Ether was fixed

--- src/ethereum-client/helper/ether-unit.go
+++ src/ethereum-client/helper/ether-unit.go
@@ -7,7 +7,7 @@ import (
 func Ether(wie *big.Int) (ether *big.Float) {
 	var a, b big.Float
 	a.SetInt(wie)
-	b.SetInt(big.NewInt(100000000000000000))
+	b.SetInt(big.NewInt(1000000000000000000))
 
 	return new(big.Float).Quo(&a, &b)
 }
